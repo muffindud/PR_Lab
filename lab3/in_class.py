dcfdab221c43baa25463ed6b9f2ff452964bda57
muffindud <ccatlabuga2002@gmail.com>
2023-09-28 08:49:50 +0300

    create basic webccrawler

--- lab3/in_class.py
+++ lab3/in_class.py
@@ -2,16 +2,40 @@ import requests
 import bs4 as bs
 
 
-link = "https://999.md/ru/list/real-estate/apartments-and-rooms?o_30_241=894&applied=1&eo=12900&eo=12912&eo=12885&eo=13859&ef=32&ef=33&o_33_1=776&page="
-max_page = 3
+absolute_home_link = "https://999.md"
+relative_link = "/ro/list/real-estate/apartments-and-rooms?o_30_241=894&applied=1&eo=12900&eo=12912&eo=12885&eo=13859&ef=32&ef=33&o_33_1=776&page="
+max_pages = 3
 
 
-def get_links(page_link, page):
-    ...
+def get_links(page_link, page, walked_pages=1) -> list:
+    # Get page content
+    response = requests.get(page_link + str(page))
+
+    # Store links
+    links = []
+
+    # Parse page content
+    soup = bs.BeautifulSoup(response.text, "html.parser")
+
+    # Find all the ad links
+    for link in soup.find_all("a", class_="js-item-ad"):
+        l = link.get("href")
+
+        # Check if the link is not already in the list and if it is an ad link
+        if l[0:4] == "/ro/" and absolute_home_link + l not in links:
+            links.append(absolute_home_link + l)
+
+    # If there are more pages and we haven't walked enough pages, get the links from the next page
+    if links != 0 and walked_pages < max_pages:
+        return links + get_links(page_link, page + 1, walked_pages + 1)
+
+    return links
 
 
 def main():
-    ...
+    links = get_links(absolute_home_link + relative_link, 10)
+    print(links)
+    print("Found " + str(len(links)) + " links.")
 
 
 if __name__ == '__main__':

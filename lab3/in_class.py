6b2a68fc86a527b761755c1b98c4a798d35ff5ce
muffindud <ccatlabuga2002@gmail.com>
2023-09-28 07:36:58 +0300

    setup basic lab3 space

---
+++ lab3/in_class.py
@@ -0,0 +1,18 @@
+import requests
+import bs4 as bs
+
+
+link = "https://999.md/ru/list/real-estate/apartments-and-rooms?o_30_241=894&applied=1&eo=12900&eo=12912&eo=12885&eo=13859&ef=32&ef=33&o_33_1=776&page="
+max_page = 3
+
+
+def get_links(page_link, page):
+    ...
+
+
+def main():
+    ...
+
+
+if __name__ == '__main__':
+    main()

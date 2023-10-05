1086f8e41e36ed86eb8bacc00b4f56694d7ddadc
muffindud <ccatlabuga2002@gmail.com>
2023-10-05 12:51:57 +0300

    add scraper

--- lab4/scraper.py
+++ lab4/scraper.py
@@ -4,7 +4,7 @@ import re
 import json
 
 
-url = "127.0.0.1:8080/"
+url = "http://127.0.0.1:8080"
 
 
 def find_links(response):
@@ -13,7 +13,7 @@ def find_links(response):
 
     for link in soup.find_all("a"):
         links.append(url + link.get("href")) if url + link.get('href') not in links else None
-
+    print(links)
     return links
 
 
@@ -22,15 +22,15 @@ def parse_product(response):
     product = {}
 
     for tag in soup.find_all('p'):
-        if tag.getText[0:6] == "Name: ":
+        if tag.getText()[0:6] == "Name: ":
             product["name"] = tag.getText()[6:]
-        elif tag.getText[0:8] == "Author: ":
+        elif tag.getText()[0:8] == "Author: ":
             product["author"] = tag.getText()[8:]
-        elif tag.getText[0:7] == "Price: ":
+        elif tag.getText()[0:7] == "Price: ":
             product["price"] = tag.getText()[7:]
-        elif tag.getText[0:13] == "Description: ":
+        elif tag.getText()[0:13] == "Description: ":
             product["description"] = tag.getText()[13:]
-
+    print(product)
     return product
 
 
@@ -38,24 +38,26 @@ def main():
     parsed_links = []
     new_links = [url]
 
-    while len(parsed_links) > len(new_links):
-        for link in parsed_links:
-            response = requests.get(link)
+    for link in new_links:
+        response = requests.get(link)
 
-            if response.status_code == 200:
-                if re.search(r"/product/\d+$", link):
-                    ...
-                else:
-                    f = open("dumps/{}.html".format(link), "w")
-                    f.write(response.text)
-                    f.close()
+        if response.status_code == 200:
+            if re.search(r"/product/\d+$", link):
+                product = parse_product(response)
+                f = open("dumps/{}.json".format(link.replace('/', '-')), "w")
+                f.write(json.dumps(product, indent=4))
+                f.close()
+            else:
+                f = open("dumps/{}.html".format(link.replace('/', '-')), "w")
+                f.write(response.text)
+                f.close()
 
-                page_links = find_links(response)
-                for page_link in page_links:
-                    if page_link not in new_links:
-                        new_links.append(link)
+            page_links = find_links(response)
+            for page_link in page_links:
+                if page_link not in new_links:
+                    new_links.append(page_link)
 
-                parsed_links.append(link)
+            parsed_links.append(link)
 
 
 if __name__ == '__main__':

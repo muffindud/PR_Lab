aa5b736d772ee72348830dd7069a0859e5e81da2
muffindud <ccatlabuga2002@gmail.com>
2023-11-20 14:17:28 +0200

    implement put and delete

--- lab8/routes.py
+++ lab8/routes.py
@@ -18,12 +18,14 @@ def template(scooter_id, scooter_name, scooter_battery_level):
     }
 
 
+# Get all scooters
 @app.route('/api/electro_scooters', methods=['GET'])
 def get_all_electro_scooters():
     scooters = ElectroScooter.query.all()
     return jsonify([template(scooter.id, scooter.name, scooter.battery_level) for scooter in scooters]), 200
 
 
+# Get specific scooter by id
 @app.route('/api/electro_scooters/<int:scooter_id>', methods=['GET'])
 def get_electro_scooter_by_id(scooter_id):
     scooter = ElectroScooter.query.get(scooter_id)
@@ -32,6 +34,7 @@ def get_electro_scooter_by_id(scooter_id):
     return jsonify(template(scooter.id, scooter.name, scooter.battery_level)), 200
 
 
+# Create specific scooter by id
 @app.route('/api/electro_scooters', methods=['POST'])
 def add_electro_scooter():
     try:
@@ -42,3 +45,32 @@ def add_electro_scooter():
     except:
         return jsonify({"error": "Invalid JSON"}), 400
 
+
+# Update specific scooter by id
+@app.route('/api/electro_scooters/<int:scooter_id>', methods=['PUT'])
+def update_electro_scooter(scooter_id):
+    try:
+        electro_scooter = ElectroScooter.query.get(scooter_id)
+        if electro_scooter is None:
+            return jsonify({"error": "Electro scooter not found"}), 404
+        data = request.get_json()
+        electro_scooter.name = data['name']
+        electro_scooter.battery_level = data['battery_level']
+        db.session.commit()
+        return jsonify({"message": "Electro scooter updated successfully"}), 200
+    except:
+        return jsonify({"error": "Invalid JSON"}), 400
+
+
+# Delete specific scooter by id
+@app.route('/api/electro_scooters/<int:scooter_id>', methods=['DELETE'])
+def delete_electro_scooter(scooter_id):
+    try:
+        electro_scooter = ElectroScooter.query.get(scooter_id)
+        if electro_scooter is None:
+            return jsonify({"error": "Electro scooter not found"}), 404
+        db.session.delete(electro_scooter)
+        db.session.commit()
+        return jsonify({"message": "Electro scooter deleted successfully"}), 200
+    except:
+        return jsonify({"error": "Invalid JSON"}), 400

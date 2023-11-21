8ce25ac6d78cc4c5f362527a201ff3c77bc856ee
muffindud <ccatlabuga2002@gmail.com>
2023-11-21 12:22:07 +0200

    implement basic RAFT binding

--- lab8/src/RAFT.py
+++ lab8/src/RAFT.py
@@ -8,60 +8,51 @@ class RAFT:
     def __init__(
             self,
             host: str,
-            port: str,
-            followers_ct: int,
-            params: dict
+            port: int,
+            instance_ct: int,
+            server_params: dict,
     ):
         self.host: str = host
-        self.port: str = port
-        self.followers_ct: int = followers_ct
+        self.port: int = port
+        self.instance_ct: int = instance_ct
+        self.server_params: dict = server_params
 
-        # UDP socket object
-        self.udp_socket: socket = socket(AF_INET, SOCK_DGRAM)
+        self.socket: socket = socket(AF_INET, SOCK_DGRAM)
 
+        # Bind socket to host and an available port
         try:
-            # Try to acquire master role
-            self.role: str = "master"
+            # Successful bind will assign "main" role
+            self.socket.bind((self.host, self.port))
+            self.role: str = "main"
+            print("Successfully bound socket to host and port, assigning main role")
 
-            # Bind UDP socket to host and port
-            self.udp_socket.bind((self.host, self.port))
+            self.backup_params: list = []
+            while len(self.backup_params) < self.instance_ct - 1:
+                print("Awaiting backup params from backup server")
+                msg, addr = self.socket.recvfrom(1024)
+                msg = msg.decode()
 
-            # Store the followers in a list
-            self.slave_params: list = []
-            msg_ct: int = 0
-
-            # Listen for messages from slave RAFT nodes
-            while True:
-                # Receive message from UDP socket
-                msg, addr = self.udp_socket.recvfrom(1024)
-
-                # If message is ping, send params
-                if msg.decode() == "ping":
-                    # If message is ping, send params
-                    msg_ct += 1
-                    self.udp_socket.sendto(
-                        str.encode(dumps(params)),
-                        addr
-                    )
+                if msg == "ack":
+                    # Wait for "ack" message from client
+                    # Send server params to client
+                    self.socket.sendto(dumps(self.server_params).encode(), addr)
                 else:
-                    # If message is params, store slave params
-                    msg_ct += 1
-                    follower_params = loads(msg.decode())
-                    self.slave_params.append(follower_params)
+                    # Received "backup" params
+                    self.backup_params.append(loads(msg))
+                    print("Received backup params from backup server: ", msg)
 
-                # If all followers have been stored, stop listening
-                if msg_ct == self.slave_params * 2:
-                    break
         except:
-            # If master role is not available, acquire slave role
-            self.role: str = "slave"
+            print("Failed to bind socket to host and port, assigning backup role")
 
-            # Send ping to master
-            self.udp_socket.sendto(str.encode("ping"), (self.host, self.port))
-            self.master_params: dict = loads(self.udp_socket.recv(1024).decode())
+            # Unsuccessful bind will assign "backup" role
+            self.role: str = "backup"
 
-            # Send params to master
-            self.udp_socket.sendto(
-                str.encode(dumps(params)),
-                (self.host, self.port)
-            )
+            # Send "ack" message to server
+            self.socket.sendto("ack".encode(), (self.host, self.port))
+
+            # Receive server params from server
+            self.main_params: dict = loads(self.socket.recvfrom(1024)[0].decode())
+            print("Received main params from main server: ", self.main_params)
+
+            # Send "backup" params to server
+            self.socket.sendto(dumps(self.server_params).encode(), (self.host, self.port))
